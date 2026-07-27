9585c1ec17e381929de2ba610327224e2350f859
x3hy <tkf.x1os@gmail.com>
2026-07-28 00:40:50 +1200

    Added dropdown menu behavior to mobile

--- prototypes/page/src/index.js
+++ prototypes/page/src/index.js
@@ -12,10 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
 				</a>
 			</header>
 			<div class="nav-buttons">
-				<a href="" title="View Posts"><button>Posts <i>(Public)</i></button></a>
-				<a href="" title="Log in"><button>Login</button></a>
-				<a href="" title="Sign up"><button class="alt">Sign Up</button></a>
-				<a href="" title="Create Post"><button class="alt">+</button></a>
+				<!-- Hamburger icon is ez to do in css -->
+				
+				<div class="hamburger" tabindex="0">
+					<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M4 7L7 7M20 7L11 7" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+						<path d="M20 17H17M4 17L13 17" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+						<path d="M4 12H7L20 12" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+					</svg>
+				</div>
+				<div class="dropdown-buttons">
+					<a href="" title="View Posts"><button>Posts <i>(Public)</i></button></a>
+					<a href="" title="Log in"><button>Login</button></a>
+					<a href="" title="Sign up"><button class="alt">Sign Up</button></a>
+					<a href="" title="Create Post"><button class="alt">+</button></a>
+				</div>
 			</div>
 		`
 	}
