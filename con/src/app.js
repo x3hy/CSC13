34498b3fe6565c2b1ca76564b7ed728244ef87bc
x3hy <tkf.x1os@gmail.com>
2026-08-31 10:58:49 +1200

    Added more meta

--- con/src/app.js
+++ con/src/app.js
@@ -4,23 +4,22 @@ const cart_clear = document.getElementById("cart-clear");
 const cart_list = document.getElementById("receipt");
 const prod = document.getElementById("product-list");
 
+	// Create the receipt list + add quants to components
 // Quantitys are kept in here
 let quants = [];
 
-// Clear all of the quantitys in the array
+
+// Remove all index quantitys
 cart_clear.addEventListener("click", () => {
-	quants.forEach(q => {
-		q["quant"] = 0;
-		q["element"].getElementsByClassName("total")[0]
-			.innerText = 0;
-	});
-	
+	quants.forEach(q => q["quant"] = 0);
 	update_cart();
 	return;
 });
 
+
 // Updates the total items and total price in the cart
 function update_cart(){
+
 	// Sum prices*quantitys accross all elements
 	const cost = cart.getElementsByClassName("price")[0];
 	cost.innerText = format_price(quants.reduce((sum, item) =>
@@ -34,13 +33,39 @@ function update_cart(){
 		, 0);
 
 	cart_list.innerHTML = ""
-	quants.forEach(q=>{
-		if (q["quant"] != 0)
-			cart_list.innerHTML +=
-				`<p>x${q["quant"]} ${q["title"]} <i>(${format_price(q["quant"] *  q["price"])})</i></p>`
-	})
+	quants.forEach((q, i) =>{
+		
+		// Update component quantitys
+		q["element"].getElementsByClassName("total")[0]
+			.innerText = q["quant"];
 
-	return;
+		// Create rows in receipt
+		if (q["quant"] != 0){
+			const item = document.createElement("p");
+			item.innerHTML = `x${q["quant"]} ${q["title"]} <i>(${format_price(q["quant"] *  q["price"])})</i>`
+			cart_list.appendChild(item);
+
+			const btn_dec = document.createElement("button");
+			btn_dec.innerText = "-";
+
+			const btn_inc = document.createElement("button");
+			btn_inc.innerText = "+";
+
+			btn_dec.addEventListener("click", () => {
+				quants[i]["quant"]--;
+				update_cart();
+			});
+
+			btn_inc.addEventListener("click", () => {
+				quants[i]["quant"]++;
+				update_cart();
+			});
+
+			item.appendChild(btn_dec);
+			item.innerHTML += "<span class=\"sep\"></span>";
+			item.appendChild(btn_inc);
+		}
+	})
 }
 
 
@@ -49,13 +74,6 @@ prod.querySelectorAll("article").forEach((el, i) => {
 	const quant_max = Number(el.getAttribute("data-max-amount"));
 	const btn_inc = el.getElementsByClassName("increase")[0];
 	const btn_dec = el.getElementsByClassName("decrease")[0];
-	const quant_val = el.getElementsByClassName("total")[0];
-
-	// Update the individual components amount
-	function update_quant(){
-		quant_val.innerText = quants[i]["quant"];
-		update_cart();
-	}
 
 	// Create a new indice in the array
 	quants.push({
@@ -71,15 +89,14 @@ prod.querySelectorAll("article").forEach((el, i) => {
 			return;
 
 		quants[i]["quant"]++;
-		update_quant();
+		update_cart();
 	});
 
 	// Decriment quantity if its above 0
 	btn_dec.addEventListener("click", () => {
 		if (quants[i]["quant"] > 0){
 			quants[i]["quant"]--;
-			update_quant();
-			return;
+			update_cart();
 		}
 	});
 })
